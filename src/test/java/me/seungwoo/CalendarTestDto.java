22449c313014566710012235644e712f2f4dc265
sonseungwoo <skyghy3342@naver.com>
2019-04-05 23:13:26 +0900

    commit

--- src/test/java/me/seungwoo/CalendarTestDto.java
+++ src/test/java/me/seungwoo/CalendarTestDto.java
@@ -17,7 +17,7 @@ public class CalendarTestDto {
 
     //현재+1일 부터 1년치 달력 데이터
     @Data
-    public static class CalendarData{
+    public static class CalendarData {
         private List<MonthData> list;
         private ItemData item;
         private String weekdayTitle[] = new String[]{"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
@@ -25,7 +25,7 @@ public class CalendarTestDto {
 
     //월별 담기
     @Data
-    public static class MonthData{
+    public static class MonthData {
         private int year;
         private int month;
         private List<List<DayData>> days;
@@ -34,7 +34,7 @@ public class CalendarTestDto {
     @Data
     @AllArgsConstructor
     @NoArgsConstructor
-    public static class ItemData{
+    public static class ItemData {
         private LocalDate firstDate;
         private LocalDate lastDate;
     }
@@ -43,7 +43,7 @@ public class CalendarTestDto {
     @Data
     @AllArgsConstructor
     @NoArgsConstructor
-    public static class DayData{
+    public static class DayData {
         private int day;
         private Boolean isPastMonth;
         private Boolean selectable;
