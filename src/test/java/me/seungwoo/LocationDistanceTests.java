49ecd8874e35f24a4b0c6cda3cf8be5b9aac06eb
sonseungwoo <skyghy3342@naver.com>
2019-04-08 21:05:40 +0900

    commit

--- src/test/java/me/seungwoo/LocationDistanceTests.java
+++ src/test/java/me/seungwoo/LocationDistanceTests.java
@@ -17,47 +17,16 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @SpringBootTest
 public class LocationDistanceTests {
 
-
     private static final Logger logger = LoggerFactory.getLogger(LocationDistanceTests.class);
 
     @Test
     public void distanceTest() {
         // 마일(Mile) 단위
         //LATITUDE:위도, LONGITUDE:경도
-        double distanceMile =
-                distance(37.504198, 127.047967, 37.501025, 127.037701, "");
-
-        // 미터(Meter) 단위
-        double distanceMeter =
-                distance(37.504198, 127.047967, 37.501025, 127.037701, "meter");
-
-        // 킬로미터(Kilo Meter) 단위
         double distanceKiloMeter =
-                distance(37.504198, 127.047967, 37.501025, 127.037701, "kilometer");
-
-        logger.info("distanceMile : {}", distanceMile);
-        logger.info("distanceMeter : {}", distanceMeter);
+                calDistance(37.504198, 127.047967, 37.501025, 127.037701);
         logger.info("distanceKiloMeter : {}", distanceKiloMeter);
     }
-
-    private static double distance(double lat1, double lon1, double lat2, double lon2, String unit) {
-
-        double theta = lon1 - lon2;
-        double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
-
-        dist = Math.acos(dist);
-        dist = rad2deg(dist);
-        dist = dist * 60 * 1.1515;
-
-        if (unit == "kilometer") {
-            dist = dist * 1.609344;
-        } else if (unit == "meter") {
-            dist = dist * 1609.344;
-        }
-
-        return (dist);
-    }
-
 
     // This function converts decimal degrees to radians
     private static double deg2rad(double deg) {
@@ -67,6 +36,22 @@ public class LocationDistanceTests {
     // This function converts radians to decimal degrees
     private static double rad2deg(double rad) {
         return (rad * 180 / Math.PI);
+    }
+
+
+    private static double calDistance(double lat1, double lon1, double lat2, double lon2){
+        double theta, dist;
+        theta = lon1 - lon2;
+        dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1))
+                * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+
+        dist = dist * 60 * 1.1515;
+        dist = dist * 1.609344;     // 단위 mile 에서 km 변환.
+        //dist = dist * 1000.0;     // 단위  km 에서 m 로 변환
+
+        return dist;
     }
 
 }
