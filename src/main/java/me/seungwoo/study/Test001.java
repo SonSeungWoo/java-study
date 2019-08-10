751e59fcb2ff92339043b6ea234029e26f670c51
sonseungwoo <skyghy3342@naver.com>
2019-08-10 21:08:52 +0900

    commit

---
+++ src/main/java/me/seungwoo/study/Test001.java
@@ -0,0 +1,16 @@
+package me.seungwoo.study;
+
+/**
+ * Created by Leo.
+ * User: sonseungwoo
+ * Date: 2019-07-14
+ * Time: 14:41
+ */
+public abstract class Test001 {
+
+    abstract String test();
+
+    String test002() {
+        return "test";
+    }
+}
